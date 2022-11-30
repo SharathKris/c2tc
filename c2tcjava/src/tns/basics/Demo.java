13c941b38f136af703379454e04e9d55076db3e5
shara <shara@DESKTOP-HO5VN09>
2022-11-30 19:10:35 +0530

    Commit

--- c2tcjava/src/tns/basics/Demo.java
+++ c2tcjava/src/tns/basics/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Core Java 8");
-		System.out.println("Streamapi,collections");
+		System.out.println("Let's get started");
 	}
 }
